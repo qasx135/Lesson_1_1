3f18961755af9454d0eee1b4512bacd47980ad43
qasx135 <qasx135@gmail.com>
2023-07-04 13:07:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson_1_1/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.lesson_1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
