7b2091d50a2e637b35c86eaefef44fdf398db4c3
qasx135 <qasx135@gmail.com>
2023-07-04 13:10:49 +0300

    Changed for commit

--- app/src/main/java/com/example/lesson_1_1/MainActivity.java
+++ app/src/main/java/com/example/lesson_1_1/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.lesson_1_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+//import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     public static void main(String[] args){
         System.out.println("Hello world");
+        System.out.println("Changes to commit)");
     }
 }
